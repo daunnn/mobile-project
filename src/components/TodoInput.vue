77cc4346ae54a668d4e25d52e36dbeb9e914754a
daun Kim <daun@seoultech.ac.kr>
2022-04-03 01:47:57 +0900

    category update
    diet/exercise category modal update

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,28 +4,70 @@
 
     <modal v-if="popup">
       <span slot="footer">식단, 운동 중 하나를 선택하세요.
+        <h1></h1>  
         <button @click="diet" type="button">식단</button>
         <button @click="exer" type="button">운동</button>
-        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+ 
+
+       <button @click="popup = false">
+        <i class="addBtn fas fa-times" aria-hidden="true"></i>
+       </button>
+
       </span>
     </modal>
 
-
+<!-- 식단 입력 부분 -->
     <modal v-if="showDiet" @close="showDiet = false">
       <span slot="footer">
-        <input type="text" v-model="newTodoItem" placeholder="type" v-on:keyup="addTodo">
+        <input type="text" v-model="newTodoItem" placeholder="type" v-on:keyup.enter="addTodo">
+        <h1></h1> 
         <button @click="newTodoItem.item=='아침'">아침</button>
         <button @click="newTodoItem.item=='점심'">점심</button>
         <button @click="newTodoItem.item=='저녁'">저녁</button>
+
+        <button class="addContainer" v-on:click="addTodo">
+           <i class="addBtn fas fa-check" aria-hidden="true"></i>
+        </button> 
+
+        <button @click="showDiet = false">
+          <i class="addBtn fas fa-times" aria-hidden="true"></i>
+          </button>
+      </span>
+    </modal>
+
+<!-- 운동 입력 부분 -->
+      <modal v-if="showExercise" @close="showExercise = false">
+      <span slot="footer">
+        <input type="text" v-model="newTodoItem" placeholder="type" v-on:keyup.enter="addTodo">
+        <h1></h1> 
+        <button @click="newTodoItem.item=='유산소'">유산소</button>
+        <button @click="newTodoItem.item=='무산소'">무산소</button>
+        <button @click="newTodoItem.item=='스트레칭'">스트레칭</button>
+
+        <button class="addContainer" v-on:click="addTodo">
+           <i class="addBtn fas fa-check" aria-hidden="true"></i>
+        </button> 
+
+        <button @click="showExercise = false" >
+          <i class="addBtn fas fa-times" aria-hidden="true"></i>
+          </button>
+      </span>
+    </modal>  
+
+<!-- 아무것도 입력 안했을 때 경고 부분 -->
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">할 일을 입력하세요.
         <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
       </span>
     </modal>
+
+
   </div>
 </template>
 
 <script>
 import Modal from './common/AlertModal.vue'
-
 export default {
   data() {
     return {
@@ -44,21 +86,23 @@ export default {
         this.clearInput();
       } else {
         this.showModal = !this.showModal;
+
       }
     },
     clearInput() {
       this.newTodoItem = '';
     },
-
     DietExercise(){
       this.popup=!this.popup;
     },
     diet(){
       this.showDiet=!this.showDiet;
       this.popup = !this.popup;
+    },
+    exer(){
+      this.showExercise=!this.showExercise;
+      this.popup = !this.popup;
     }
-
-
   },
   components: {
     Modal: Modal
@@ -70,6 +114,7 @@ export default {
 input:focus {
   outline: none;
 }
+
 .inputBox {
   background: white;
   height: 50px;
