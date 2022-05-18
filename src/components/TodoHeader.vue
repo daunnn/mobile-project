bffbb10582bb875a1b95a84d30766f885797127a
Oh jiyeon <dhwldus0906@seoultech.ac.kr>
2022-05-18 22:48:16 +0900

    Edit Design
    Edit Design of page

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -2,7 +2,7 @@
   <header>
     <v-tool-bar @click="convert_main" color=rgb(236,242,255)>
       <div style="float: left; margin: 15px"> {{today_info}} </div>
-      <div style ="font-weight:bold; font-size: 20px; margin: 10px; position: absolute; left: 50%; transform: translateX(-50%);"> 다이어트 ToDo </div>
+      <div style ="font-weight:bold; font-size: 25px; top:5px; position: absolute; left: 50%; transform: translateX(-50%);"> 다이어트 ToDo </div>
     </v-tool-bar>
 
     
